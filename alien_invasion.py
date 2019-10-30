687c11626ca6f8774adeb6c874f6873b25b9fe06
蓝色 <hahada@aliyun.com>
2019-10-30 18:28:31 +0800

    Wordemotion

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,20 +1,27 @@
 import sys
 import pygame
 
+from settings import Settings
+
 
 def run_game():
     '''初始化游戏并创建一个屏幕对象'''
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("外星人入侵")
 
-    '''开始游戏'''
+    '''开始游戏主循环'''
     while True:
 
         '''监视键盘和鼠标事件'''
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        '''每次循环时都重绘屏幕'''
+        screen.fill(ai_settings.bg_color)
 
         '''让最近绘制的屏幕可见'''
         pygame.display.flip()
